eb75337a9bef6583f6f792228b9fd3a5930af4b8
akyrey <stein.dario@gmail.com>
2024-01-12 15:49:48 +0100

    feat: add sidenav links

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,23 @@
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/journal", label: "Journal" },
+];
 
 const DashboardLayout = ({ children }: React.PropsWithChildren) => {
   return (
     <div className="h-screen w-screen">
       <aside className="absolute left-0 top-0 h-full w-60 border-r border-black/10">
-        Mood
+        <div>Mood</div>
+        <ul>
+          {links.map(({ href, label }) => (
+            <li key={href} className="px-2 py-6 text-xl">
+              <Link href={href}>{label}</Link>
+            </li>
+          ))}
+        </ul>
       </aside>
       <div className="ml-60 h-full">
         <header className="h-16 border-b border-black/10">
